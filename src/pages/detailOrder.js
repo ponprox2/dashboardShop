d91ec4924b9e8ec5ecbba4a3f8fd59ce9b7996d0
ponprox2 <ponpham113@gmail.com>
2022-06-16 10:47:07 +0700

    update template

---
+++ src/pages/detailOrder.js
@@ -0,0 +1,33 @@
+import { filter } from 'lodash';
+import { sentenceCase } from 'change-case';
+import { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+// material
+import {
+  Card,
+  Table,
+  Stack,
+  Avatar,
+  Button,
+  Checkbox,
+  TableRow,
+  TableBody,
+  TableCell,
+  Container,
+  Typography,
+  TableContainer,
+  TablePagination,
+} from '@mui/material';
+// components
+import Page from '../components/Page';
+import Label from '../components/Label';
+import Scrollbar from '../components/Scrollbar';
+import Iconify from '../components/Iconify';
+import SearchNotFound from '../components/SearchNotFound';
+import { UserListHead, UserListToolbar, UserMoreMenu } from '../sections/@dashboard/user';
+
+function DetailOrder() {
+  return <div>123</div>;
+}
+
+export default DetailOrder;
