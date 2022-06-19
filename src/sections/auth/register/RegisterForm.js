93358db95750d69f3d4a1f9227f11e8700d5f0e1
chuongungdinh@gmail.com <chuongungdinh@gmail.com>
2022-06-19 14:02:14 +0700

    modified RegisterForm

--- src/sections/auth/register/RegisterForm.js
+++ src/sections/auth/register/RegisterForm.js
@@ -13,27 +13,58 @@ import Iconify from '../../../components/Iconify';
 export default function RegisterForm() {
   const navigate = useNavigate();
 
+  const [fullName, setFullName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [username, setUsername] = useState('');
+  const [pass, setPass] = useState('');
+  const [repass, setRepass] = useState('');
+  const [email, setEmail] = useState('');
+
   const [showPassword, setShowPassword] = useState(false);
 
   const RegisterSchema = Yup.object().shape({
-    firstName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('First name required'),
-    lastName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Last name required'),
+    fullName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Full name is required'),
+    phone: Yup.string().required('Phone number is required'),
     email: Yup.string().email('Email must be a valid email address').required('Email is required'),
-    password: Yup.string().required('Password is required'),
+    username: Yup.string().required('Username is required'),
+    pass: Yup.string().required('Password is required'),
+    repass: Yup.string().required('Repassword is required')
   });
 
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
+      fullName: '',
+      phone: '',
+      username: '',
+      pass: '',
+      repass: '',
+      email: ''
     },
     validationSchema: RegisterSchema,
     onSubmit: () => {
       navigate('/dashboard', { replace: true });
     },
   });
+
+  const handleClick = () => {
+    const data = {
+      a: fullName,
+      b: phone,
+      c: email,
+      d: username,
+      e: pass,
+      f: repass
+    };
+    async function insertAdmin() {
+      // const res = await axios.post('', data);
+      // // console.log(res.data);
+      // if (res?.data?.message === 'Auth successful' && res?.data?.role !== 'user') {
+      //   localStorage.setItem('adminInfo', JSON.stringify(res.data));
+      //   navigate('/dashboard/app');
+      // }
+    }
+    insertAdmin();
+  };
 
   const { errors, touched, handleSubmit, isSubmitting, getFieldProps } = formik;
 
@@ -45,17 +76,19 @@ export default function RegisterForm() {
             <TextField
               fullWidth
               label="Full name"
-              // {...getFieldProps('firstName')}
-              // error={Boolean(touched.firstName && errors.firstName)}
-              // helperText={touched.firstName && errors.firstName}
+              onChange={(e) => { setFullName(e.target.value) }}
+              {...getFieldProps('fullName')}
+              error={Boolean(touched.fullName && errors.fullName)}
+              helperText={touched.fullName && errors.fullName}
             />
 
             <TextField
               fullWidth
               label="Phone number"
-              // {...getFieldProps('lastName')}
-              // error={Boolean(touched.lastName && errors.lastName)}
-              // helperText={touched.lastName && errors.lastName}
+              onChange={(e) => { setPhone(e.target.value) }}
+              {...getFieldProps('phone')}
+              error={Boolean(touched.phone && errors.phone)}
+              helperText={touched.phone && errors.phone}
             />
           </Stack>
           <TextField
@@ -63,17 +96,20 @@ export default function RegisterForm() {
             autoComplete="username"
             // type=""
             label="User name"
-            // {...getFieldProps('email')}
-            // error={Boolean(touched.email && errors.email)}
-            // helperText={touched.email && errors.email}
+            onChange={(e) => { setUsername(e.target.value) }}
+            {...getFieldProps('username')}
+            error={Boolean(touched.username && errors.username)}
+            helperText={touched.username && errors.username}
           />
           <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
             <TextField
               fullWidth
               label="Password"
-              // {...getFieldProps('firstName')}
-              // error={Boolean(touched.firstName && errors.firstName)}
-              // helperText={touched.firstName && errors.firstName}
+              type={showPassword?'text':'password'}
+              onChange={(e) => { setPass(e.target.value) }}
+              {...getFieldProps('pass')}
+              error={Boolean(touched.pass && errors.pass)}
+              helperText={touched.pass && errors.pass}
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">
@@ -88,9 +124,10 @@ export default function RegisterForm() {
             <TextField
               fullWidth
               label="Repassword"
-              // {...getFieldProps('lastName')}
-              // error={Boolean(touched.lastName && errors.lastName)}
-              // helperText={touched.lastName && errors.lastName}
+              onChange={(e) => { setRepass(e.target.value) }}
+              {...getFieldProps('repass')}
+              error={Boolean(touched.repass && errors.repass)}
+              helperText={touched.repass && errors.repass}
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">
@@ -108,16 +145,17 @@ export default function RegisterForm() {
             autoComplete="username"
             type="email"
             label="Email address"
+            onChange={(e) => { setEmail(e.target.value) }}
             {...getFieldProps('email')}
             error={Boolean(touched.email && errors.email)}
             helperText={touched.email && errors.email}
           />
 
-          <LoadingButton fullWidth size="large" type="submit" variant="contained" loading={isSubmitting}>
+          <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleClick} loading={isSubmitting}>
             Register
           </LoadingButton>
         </Stack>
       </Form>
     </FormikProvider>
-  );
+  )
 }
